33a511434e70646fe3032d62b2047df98511a1a1
Alexey Shevchenko <otetz@me.com>
2015-06-08 02:20:22 +0300

    plot2

---
+++ plot2.R
@@ -0,0 +1,20 @@
+load_data <- function(s = 66637, n = 2880) {
+  dt <- read.csv("household_power_consumption.txt", header = FALSE, sep = ";", dec = ".", col.names = c("Date", "Time", "GActP", "GReactP", "V", "GInt", "SM1", "SM2", "SM3"), na.strings = "?", colClasses = c("character", "character", rep("numeric", times = 7)), nrows = n, skip = s)
+  dt
+}
+
+clean_data <- function(dt) {
+  x <- paste(dt$Date, dt$Time)
+  dt$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+  dt
+}
+
+make_graph2 <- function(dt) {
+  plot(dt$DateTime, dt$GActP, type="l", main = "", xlab = "", ylab = "Global Active Power (kilowatts)")
+  dev.copy(png, file = "ExData_Plotting1/plot2.png")
+  dev.off()  
+}
+
+dt <- load_data()
+dt <- clean_data(dt)
+make_graph2(dt)
